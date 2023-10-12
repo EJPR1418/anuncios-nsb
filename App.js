4b08318ed34721111aa1a79086a91be314258f4c
eperez <e.perez1418@outlook.com>
2023-10-12 11:29:15 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform} from 'react-native';
+import { useDimensions } from '@react-native-community/hooks';
+import HomeScreen from './app/screens/HomeScreen';
+
+export default function App() {
+  return (<HomeScreen></HomeScreen>);
+
+}
